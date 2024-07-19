aecfa5f3d1b69f54057ee573f636f958f836061b
Pita-Alisson <alissonpita622@gmail.com>
2024-07-18 22:31:27 -0400

    Add files via upload

---
+++ Proyecto-uni3/assets/js/script.js
@@ -0,0 +1,5 @@
+// Ejemplo: Cambiar la imagen al hacer clic
+// const personajeImg = document.querySelector('#personaje-img');
+// personajeImg.addEventListener('click', () => {
+//     personajeImg.src = 'nueva-imagen.jpg'; Cambia la imagen aquí
+// });
